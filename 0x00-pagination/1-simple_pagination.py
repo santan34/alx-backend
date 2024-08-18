58e95f6612c7d547b290c80dff8c5df9b5a8c435
St <aaronndas@gmail.com>
2024-08-19 01:42:00 +0200

    1

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,1 +1,48 @@
 #!/usr/bin/env python3
+"""
+pagination 1
+"""
+import csv
+import math
+from typing import List, Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple:
+    """
+    return a tuple of a start index and an end index
+    """
+    start_index: int = (page - 1) * page_size
+    end_index: int = start_index + page_size
+    return (start_index, end_index)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        return a pagination
+        """
+        assert isinstance(page, type(1)) and isinstance(page, type(page_size))
+        assert page > 0 and page_size > 0
+        tup: Tuple = index_range(page, page_size)
+        data = self.dataset()
+        if page_size > len(data):
+            return []
+        return data[tup[0]:tup[1]]
