ec9f8107b269b5a7e359833d55d04a38e9a38805
St <aaronndas@gmail.com>
2024-09-08 12:58:43 +0200

    0

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Basic dictionary
+"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+class BasicCache(BaseCaching):
+    """the class for baseCaching"""
+
+    def put(self, key, item):
+        """putting to a cache"""
+        if key and item:
+            self.cache_data[key] = item 
+
+    
+    def get(self, key):
+        """get a value from the cache"""
+        if key and key in self.cache_data.keys():
+            return self.cache_data.get(key)
+        return None
+
