714e39f29d9c3d56844271e27a499e11864265bb
St <aaronndas@gmail.com>
2024-09-26 03:24:27 +0200

    5

---
+++ 0x03-queuing_system_in_js/5-publisher.js
@@ -0,0 +1,22 @@
+import { createClient } from 'redis';
+
+const client = createClient();
+client.on('error', (err) => {
+  console.log(`Redis client not connected to server: ${err.message}`);
+  client.quit();
+});
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+  publishMessage('Holberton Student #1 starts course', 100);
+  publishMessage('Holberton Student #2 starts course', 200);
+  publishMessage('KILL_SERVER', 300);
+  publishMessage('Holberton Student #3 starts course', 400);
+});
+
+function publishMessage (message, time) {
+  setTimeout(() => {
+    console.log(`About to send ${message}`);
+    client.publish('holberton school channel', message);
+  }, time);
+}
---
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,20 @@
+import { createClient } from 'redis';
+
+const client = createClient();
+client.on('error', (err) => {
+  console.log(`Redis client not connected to server: ${err.message}`);
+  client.quit();
+});
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+  client.subscribe('holberton school channel');
+});
+
+client.on('message', (channel, message) => {
+  console.log(`${message}`);
+  if (message === 'KILL_SERVER') {
+    client.unsubscribe(channel);
+    client.quit();
+  }
+});
